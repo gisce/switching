3b5cec9e9d1b1a7c10fce0457525f900d5856686
Carles Pey <cpey@gisce.net>
2011-10-19 18:57:55 +0200

    NEW. Constructor de message accepta ambdós text i file

--- switching/messages/message.py
+++ switching/messages/message.py
@@ -5,11 +5,14 @@ from lxml import objectify, etree
 
 class Message(object):
     """Classe base"""
-    def __init__(self, f_xml, f_xsd):
+    def __init__(self, xml, f_xsd):
         """Construeix un missatge base.
         """
         self.f_xsd = f_xsd
-        self.f_xml = f_xml
+        if isinstance(xml, file):
+            self.str_xml = xml.read()
+        else:
+            self.str_xml = xml
         self.tipus = ''
 
     def check_fpos(self):
@@ -17,14 +20,11 @@ class Message(object):
         """
         if (isinstance(self.f_xsd, file) and self.f_xsd.tell() != 0):
             self.f_xsd.seek(0)
-        if (isinstance(self.f_xml, file) and self.f_xml.tell() != 0):
-            self.f_xml.seek(0)
     
     def get_tipus_xml(self):
         """Obtenir el tipus de missatge
         """
-        self.check_fpos()
-        obj = objectify.fromstring(self.f_xml.read())
+        obj = objectify.fromstring(self.str_xml)
         self.tipus = obj.Cabecera.CodigoDelProceso
         return self.tipus
 
@@ -34,6 +34,6 @@ class Message(object):
         self.check_fpos()
         schema = etree.XMLSchema(file=self.f_xsd)
         parser = objectify.makeparser(schema=schema)
-        obj = objectify.fromstring(self.f_xml.read(), parser)
+        obj = objectify.fromstring(self.str_xml, parser)
         return obj
     
