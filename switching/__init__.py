7f9a316716cc392e3a2509c4f93c3a3fc2509c89
Carles Pey <cpey@gisce.net>
2012-03-15 18:41:44 +0100

    Bump into v0.3.0

--- switching/__init__.py
+++ switching/__init__.py
@@ -7,7 +7,7 @@ Aquesta llibreria proveeix de les classes i mètodes necessaris pel switching
 """
 import os
 
-__version__ = '0.1.0'
+__version__ = '0.3.0'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
