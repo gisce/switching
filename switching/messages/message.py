3b70e19da118cc4b1a621d4de42b35eccd7e6098
Carles Pey <cpey@gisce.net>
2012-09-07 19:09:09 +0200

    NEW passos 09 i 10

--- switching/messages/message.py
+++ switching/messages/message.py
@@ -15,8 +15,9 @@ XSD_DATA = {'F1': {'01': 'Facturacion.xsd'},
                    '05': 'ActivacionCambiodeComercializadoraSinCambios.xsd',
                    '06': 'NotificacionComercializadoraSaliente.xsd',
                    '08': 'AnulacionSolicitud.xsd',
-                   '09': ('AceptacionAnulacion.xsd', 
-                          'RechazoAnulacion.xsd')}
+                   '09': ('AceptacionAnulacion.xsd',
+                          'RechazoAnulacion.xsd'),
+                   '10': 'NotificacionComercializadoraSaliente.xsd'}
            }
 
 _ = gettext.gettext
