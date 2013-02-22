02f1a67ee29467af556f92fc5e642f3d1632c690
Carles Pey <cpey@gisce.net>
2013-02-22 16:22:27 +0100

    Bump into v1.8.7

--- switching/__init__.py
+++ switching/__init__.py
@@ -7,7 +7,7 @@ Aquesta llibreria proveeix de les classes i mètodes necessaris pel switching
 """
 import os
 
-__version__ = '1.8.6'
+__version__ = '1.8.7'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
