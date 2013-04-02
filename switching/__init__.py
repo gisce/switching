a660b37dc5d9e20ad5aaafd1355b8e5cee9f644e
Carles Pey <cpey@gisce.net>
2013-04-02 13:13:17 +0200

    BUMP to v1.9.0

--- switching/__init__.py
+++ switching/__init__.py
@@ -7,7 +7,7 @@ Aquesta llibreria proveeix de les classes i mètodes necessaris pel switching
 """
 import os
 
-__version__ = '1.8.7'
+__version__ = '1.9.0'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
