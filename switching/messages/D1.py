89a2ecba30b97c513529bceb8b1e8249d2f3ee66
bmiro <bartomeumiro@gmail.com>
2013-09-23 12:00:58 +0200

    NEW Implemented D1

--- switching/messages/D1.py
+++ switching/messages/D1.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from message import Message
+import C1
+
 
 class D1(Message):
     """Classe que implementa D1."""
 
     @property
     def notificacio_canvi(self):
-        return NotificacioCanvi(self.obj.NotificacionCambiosATRDesdeDistribuidor)
-    
+        return NotificacioCanvi(self.obj.
+                                NotificacionCambiosATRDesdeDistribuidor)
+
     @property
     def periodicitat_facturacio(self):
         periodicitat = ''
@@ -18,14 +21,19 @@ class D1(Message):
         except AttributeError:
             pass
         return periodicitat
-        
+
+    @property
+    def contracte(self):
+        """Retorna l'objecte Contracte"""
+        return False
+
 
 class NotificacioCanvi(object):
     """Classe que implementa la direccio"""
 
     def __init__(self, data):
         self.notificacio = data
-    
+
     @property
     def motiu_canvi(self):
         motiu = ''
