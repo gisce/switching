6bfeb59ab5ef07426eed5b76902480481446b34d
David García Garzón <voki@canvoki.net>
2015-05-05 19:52:26 +0200

    IMP: removing helpers already in test_helpers

--- tests/test_switching.py
+++ tests/test_switching.py
@@ -7,26 +7,9 @@ from switching.output.messages.base import Cabecera
 import os
 import unittest
 
-
-# TODO: Move this to a common module
-def assertXmlEqual(self, got, want):
-    from lxml.doctestcompare import LXMLOutputChecker
-    from doctest import Example
-
-    checker = LXMLOutputChecker()
-    if checker.check_output(want, got, 0):
-        return
-    message = checker.output_difference(Example("", want), got, 0)
-    raise AssertionError(message)
-
-unittest.TestCase.assertXmlEqual = assertXmlEqual
-unittest.TestCase.__str__ = unittest.TestCase.id
-
-
 import test_helpers
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-
 
 def get_data(path):
     return os.path.join(_ROOT, 'data', path)
