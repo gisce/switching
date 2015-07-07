1e7e9e30e29e0372333e32d19b1799b3f9aea264
Agustí Fita i Borrell <afita@gisce.net>
2015-07-07 18:55:50 +0200

    Bump to v2.6.5

--- switching/__init__.py
+++ switching/__init__.py
@@ -7,7 +7,7 @@ Aquesta llibreria proveeix de les classes i mètodes necessaris pel switching
 """
 import os
 
-__version__ = '2.6.4'
+__version__ = '2.6.5'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
