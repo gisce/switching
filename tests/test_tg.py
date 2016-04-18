208817bedb6427f95d8ce250b181d70036a9e2d6
Simó Albert i Beltran <sim6@probeta.net>
2016-04-18 12:46:17 +0200

    Dockstrings for S04 tests

--- tests/test_tg.py
+++ tests/test_tg.py
@@ -5,16 +5,34 @@ from switching.input.messages import message, TG
 
 
 class TestS04Sagecom(unittest.TestCase):
+    """
+    Test for S04 reports of Sagecom meters
+    """
 
     def setUp(self):
+        """
+        Read S04 report
+
+        :return: None
+        """
         self.xml = open(get_data('S04_0_20150504125253.xml'), "r")
         self.tg_xml = message.MessageTG(self.xml)
         self.tg_xml.parse_xml()
 
     def tearDown(self):
+        """
+        Close report file
+
+        :return: None
+        """
         self.xml.close()
 
     def test_fix_sagecom_FFFFFFFFFFFFFFFFFW(self):
+        """
+        Return epoch date for FFFFFFFFFFFFFFFFFW
+
+        :return:
+        """
         for cnc in self.tg_xml.obj.Cnc:
             concentrator = TG.Concentrator(cnc)
             values = TG.Values(None, None, None)
@@ -29,16 +47,34 @@ class TestS04Sagecom(unittest.TestCase):
 
 
 class TestS04Circutor(unittest.TestCase):
+    """
+    Test for S04 reports of Circutor meters
+    """
 
     def setUp(self):
+        """
+        Read S04 report
+
+        :return: None
+        """
         self.xml = open(get_data('CUR7937810137_0_0_20160401003006.xml'), "r")
         self.tg_xml = message.MessageTG(self.xml)
         self.tg_xml.parse_xml()
 
     def tearDown(self):
+        """
+        Close report file
+
+        :return: None
+        """
         self.xml.close()
 
     def test_fix_circutor_Fhi_00000000000000000W(self):
+        """
+        Return epoch date for 00000000000000000W
+
+        :return:
+        """
         for cnc in self.tg_xml.obj.Cnc:
             concentrator = TG.Concentrator(cnc)
             values = TG.Values(None, None, None)
