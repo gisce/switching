d4049aee90b4d8acb25adfe7b99ec340b3bf1001
Eduard Berloso <eberloso@gisce.net>
2016-08-31 13:58:05 +0200

    Added MinimumFieldsChecker class

--- switching/input/messages/R1.py
+++ switching/input/messages/R1.py
@@ -747,3 +747,124 @@ class Intervencion(object):
             pass
         return ref
 
+
+class MinimumFieldsChecker(object):
+    def check_nif_cliente(self, r1):
+        return getattr(getattr(r1, 'client', None), 'codi_identificacio', None)
+
+    def check_nombre_cliente(self, r1):
+        return getattr(getattr(r1, 'client', None), 'nom', None)
+
+    def check_telefono_contacto(self, r1):
+        for var in r1.reclamacions:
+            if not getattr(getattr(var, 'contacto', None), 'telf_num', None):
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_cups(self, r1):
+        return r1.cups
+
+    def check_fecha_incidente(self, r1):
+        for var in r1.reclamacions:
+            if not var.data_incident:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_comentarios(self, r1):
+        return r1.comentaris
+
+    def check_codigo_incidencia(self, r1):
+        for var in r1.reclamacions:
+            if not var.codi_incidencia:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_persona_de_contacto(self, r1):
+        for var in r1.reclamacions:
+            if not var.contacto:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_num_fact(self, r1):
+        for var in r1.reclamacions:
+            if not var.num_factura_atr:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_tipo_concepto_facturado(self, r1):
+        for var in r1.reclamacions:
+            if not var.tipus_concepte_facturat:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_lectura(self, r1):
+        for var in r1.reclamacions:
+            if len(var.lectures) == 0:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_fecha_de_lectura(self, r1):
+        for var in r1.reclamacions:
+            if not var.data_lectura:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_fecha_desde(self, r1):
+        for var in r1.reclamacions:
+            if not var.data_inici:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_fecha_hasta(self, r1):
+        for var in r1.reclamacions:
+            if not var.data_fins:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_ubicacion_incidencia(self, r1):
+        for var in r1.reclamacions:
+            if not var.ubicacio:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_codigo_de_solicitud(self, r1):
+        for var in r1.reclamacions:
+            if not var.codi_sollicitud:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_concepto_contratacion(self, r1):
+        for var in r1.reclamacions:
+            if not var.param_contractacio:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_cta_banco(self, r1):
+        for var in r1.reclamacions:
+            if not var.iban:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_sol_nuevos_suministro(self, r1):
+        for var in r1.reclamacions:
+            if not var.numexpedient:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_cod_reclam_anterior(self, r1):
+        for var in r1.reclamacions:
+            if not var.codi_sollicitud_reclamacio:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_importe_reclamado(self, r1):
+        for var in r1.reclamacions:
+            if not var.import_reclamat:
+                return False
+        return len(r1.reclamacions) > 0
+
+    def check_tipo_atencion_incorrecta(self, r1):
+        for var in r1.reclamacions:
+            if not var.tipus_atencio_incorrecte:
+                return False
+        return len(r1.reclamacions) > 0
