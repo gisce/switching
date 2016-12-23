fff19fdb72eb743b0337be8cb4974558b19b4f00
Eduard Berloso <eberloso@gisce.net>
2016-12-23 09:45:40 +0100

    Rewrite table C2

--- switching/defs.py
+++ switching/defs.py
@@ -1397,21 +1397,17 @@ TERMINIS_A3 = ({
     '06': [5, 'DA', '07'],  # Tenim 5 dies per acceptar o rebutjar anulacio (enviar pas 07)
 })
 
+TERMINIS_C2 = ({
+    '01': [5, 'DA', '02'],  # Tenim 5 dies per acceptar o rebutjar (enviar pas 02)
+    '02': [1, 'DA', '05'],  # Tenim 1 dia per enviar comunicacio de activació des que s'activen canvis. Mentres no s'activen tenim 2 mesos per fer-ho.
+    '03': [30, 'DN', '05'],  # En 30 dies s'ha d'haver sol.lucionat la incidencia (enviar 05), altrament es rebutja (enviar 04)
+    '08': [5, 'DA', '09'],  # Tenim 5 dies per acceptar o rebutjar anulacio (enviar pas 09)
+})
+
+
 # Terminis dels processos de switching OLD.
 # Estructura: 'pas a enviar': ['dies de termini', 'tipus de dies']
 # Tipus de dies: DA -> dies habils, DN -> dies naturals
-TERMINIS_C2 = ({
-    '02': ['5', 'DA'],  # desde la recepcio de la sol.licitud (01)
-    '04_no_incidencia': ['15', 'DN'],  # desde acceptacio de la sol.licitud (02)
-    '04_incidencia': ['30', 'DN'],  # desde acceptacio de la sol.licitud (02)
-    '05': ['1', 'DA'],  # desde la activacio
-    '06': ['1', 'DA'],  # desde la activacio, simultani a 05
-    '09': ['5', 'DA'],  # desde la recepcio de la sol.licitud d'anul.lacio (08)
-    '10': ['5', 'DA'],  # simultani a 09
-    '11': ['5', 'DA'],  # simultani a 02
-    '12_no_incidencia': ['15', 'DN'],  # simultani a 04_no_incidencia
-    '12_incidencia': ['30', 'DN'],  # simultani a 04_incidencia
-})
 
 # Terminis per el proces B1 amb motiu 01
 TERMINIS_B1_01 = ({
